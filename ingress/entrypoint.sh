34836f54a8361e147198bcb88219711983218f2f
r3xdj <redred651204@gmail.com>
2026-08-12 21:16:06 +0800

    完成！(說明文件還沒啦XD)

--- ingress/entrypoint.sh
+++ ingress/entrypoint.sh
@@ -1,4 +1,14 @@
 #!/bin/sh
+
+set -eu
+
+# ------------------------------------------------------------
+# Apache flag
+# ------------------------------------------------------------
+
+chown opadmin:opadmin /flag.txt
+chmod 644 /flag.txt
+
 echo "opadmin:${SSH_PIVOT_PASSWORD}" | chpasswd
 /usr/sbin/sshd
 exec httpd-foreground
